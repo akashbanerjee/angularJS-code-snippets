4f16f88b69e395ef2eadc0f09f191eb687415e2a
Akash Banerjee <akashbnrj07@gmail.com>
2017-04-14 13:57:00 +0530

    Changes for validations

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,15 +7,32 @@
 
   function LunchCheckController($scope) {
     $scope.name = "";
+    $scope.messages = "";
 
-    $scope.check = function() {
-        var numberOfTypes = $scope.name.split(',');
-        if(numberOfTypes.length=0)
-          $scope.message = "Please enter data first";
-        else if(numberOfTypes.length<=3)
-          $scope.message = "Enjoy!";
+    $scope.check = function() { 
+
+      var input = $scope.name;
+       var numberOfTypes = input.split(",");
+       var numberOfItems = 0;
+       console.log(numberOfTypes.length);
+       for (var i = 0; i < numberOfTypes.length; i++) {
+           if (numberOfTypes[i].trim() != "") {
+               numberOfItems ++;
+           }
+       }
+         
+        if(input.trim() == 0)
+        {
+          $scope.messages = "Please enter data first";
+        }
+        else if(numberOfItems <= 3)
+        {
+          $scope.messages = "Enjoy!";
+        }
         else
-          $scope.message = "Too much!";
+        {
+          $scope.messages = "Too much!";
+        }
     };
 
   };
